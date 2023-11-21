8b6400758d3c9727d4b73281af20eb9698899a12
Nicolas Berthou <berthou.nicolas@laposte.net>
2023-11-21 20:57:49 +0100

    COMMENT WIP

--- src/classes/Guild.ts
+++ src/classes/Guild.ts
@@ -1,42 +1,42 @@
-import { Wallet } from './Wallet';
-import { prismaClient } from '../utils';
+// import { Wallet } from './Wallet';
+// import { prismaClient } from '../utils';
 
-export enum GuildName {
-  FUSEES = 'Fusées',
-  PLANETES = 'Planètes',
-  COMETES = 'Comètes',
-}
+// export enum GuildName {
+//   FUSEES = 'Fusées',
+//   PLANETES = 'Planètes',
+//   COMETES = 'Comètes',
+// }
 
-export class Guild {
-  public name: string;
-  public bank: Wallet;
-  private userId?: string;
+// export class Guild {
+//   public name: string;
+//   public bank: Wallet;
+//   private userId?: string;
 
-  constructor(userId?: string) {
-    this.name = '';
-    this.bank = new Wallet('');
-    this.userId = userId;
-  }
+//   constructor(userId?: string) {
+//     this.name = '';
+//     this.bank = new Wallet('');
+//     this.userId = userId;
+//   }
 
-  public async init(): Promise<Guild | void> {
-    const guild = await prismaClient.guild.findFirst({
-      where: {
-        members: {
-          some: {
-            id: this.userId,
-          },
-        },
-      },
-    });
-    if (guild) {
-      this.name = guild.name;
-      this.bank = await new Wallet(guild.id, 'GUILD').init();
-      return this;
-    }
-  }
+//   public async init(): Promise<Guild | void> {
+//     const guild = await prismaClient.guild.findFirst({
+//       where: {
+//         members: {
+//           some: {
+//             id: this.userId,
+//           },
+//         },
+//       },
+//     });
+//     if (guild) {
+//       this.name = guild.name;
+//       this.bank = await new Wallet(guild.id, 'GUILD').init();
+//       return this;
+//     }
+//   }
 
-  public async getGuilds() {
-    const guilds = await prismaClient.guild.findMany();
-    return guilds;
-  }
-}
+//   public async getGuilds() {
+//     const guilds = await prismaClient.guild.findMany();
+//     return guilds;
+//   }
+// }
